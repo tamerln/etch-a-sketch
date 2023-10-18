ee3a050a8c44bc91ec7badb6837f1a3cdba91b0d
tamerln <benebin@protonmail.com>
2023-10-17 19:33:40 -0600

    wrote basic code for etch-a-sketch with my very first use of nested for loops git status git status
    fds
    f
    as
    asdf
    \dasf
    asF
    asf
    ads
    fasas

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector("#container");
+
+
+for (let i = 1; i < 16; i++) {
+let rows = document.createElement('div');
+rows.classList.add('rows');
+container.appendChild(rows);
+    for (let j = 0; j < 16; j++) {
+        let cells = document.createElement('div');
+        rows.appendChild(cells);
+        cells.classList.add('cells');
+        document.addEventListener("mouseover", function(event) {
+            event.target.style.backgroundColor = 'yellow';
+            })
+     
+    }
+}
